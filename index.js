101828b8c10390c064e1e6b98206fda20c9ae232
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-08-07 11:02:20 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(Aki){
+    return `Hi, my name is ${Aki}.`;
+}
+function introductionWithLanguage(Aki,JavaScript){
+  return `Hi, my name is ${Aki} and I am learning to program in ${JavaScript}.`;
+}
+function introductionWithLanguageOptional(Aki,language = "JavaScript"){
+return `Hi, my name is ${Aki} and I am learning to program in ${language}.` ;
+}
